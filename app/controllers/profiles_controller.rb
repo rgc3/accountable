0b66a25e62f3c2723d281d98d972c589cb822fb9
Robert Cunningham <rgc3dev@gmail.com>
2016-02-26 22:09:55 -0500

    Configure profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,16 @@
 class ProfilesController < ApplicationController
-  def index
+  def show
+    #This sets @user to a user based on their id
+    @user = User.find(params[:id])
   end
 
   def feed
+    @goals = Goal.where("user_id in (?) OR user_id = ?", current_user.friend_ids, current_user).order('created_at DESC')
+    @goal = Goal.new
   end
 
-  def show
+  def index
+    #This sets @users to every User in our database
+    @users = User.all
   end
 end
