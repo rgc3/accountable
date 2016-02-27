27c2f096858d37ef8959aba54aae6da09a4585b3
Robert Cunningham <rgc3dev@gmail.com>
2016-02-27 08:37:28 -0500

    Add username validations to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates :username, presence: true, uniqueness: true
 
   has_many :goals
   has_many :comments
