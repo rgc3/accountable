22b74e8a2e13ee5f02dbb6b095340b4e9c5cc87b
Robert Cunningham <rgc3dev@gmail.com>
2016-02-27 08:59:46 -0500

    Add missing following/followers code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   validates :username, presence: true, uniqueness: true
 
   has_many :goals
   has_many :comments
   has_many :relationships
   has_many :friends, through: :relationships
+
   has_many :inverse_relationships, class_name: "Relationship", foreign_key: "friend_id"
   has_many :inverse_friends, through: :inverse_relationships, source: :user
 
