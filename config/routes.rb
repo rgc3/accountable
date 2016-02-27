b6a9647e85baf151cb017b97fa6e7b555292d964
Robert Cunningham <rgc3dev@gmail.com>
2016-02-26 22:03:16 -0500

    configure application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+  resources :relationships
+  resources :comments
+  resources :goals
+  devise_for :users
+  
   get 'profiles/index'
 
   get 'profiles/feed'
 
   get 'profiles/show'
-
-  resources :relationships
-  resources :comments
-  resources :goals
-  devise_for :users
 
   root 'goals#index'
 
