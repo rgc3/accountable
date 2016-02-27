695426d5991244483df4f7823a31ae67c8777c1d
Robert Cunningham <rgc3dev@gmail.com>
2016-02-26 21:44:49 -0500

    Associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ActiveRecord::Base
+  belongs_to :goal
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :goals
+  has_many :comments
+  has_many :relationships
+  has_many :friends, through: :relationships
+  has_many :inverse_relationships, class_name: "Relationship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_relationships, source: :user
+
   has_attached_file :avatar, styles: { medium: "300x300>", small: "200x200>", thumb: "100x100>", navpic: "30x30>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
